bd70538a1f1c60b9be4065c98f565492030a3e41
Arslaan-Thanedar <arslaanthanedar@gmail.com>
2023-10-19 20:33:06 +0530

    first script

---
+++ scripts/nginx_high_memory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the Nginx web server's memory usage
+nginx_memory_usage=$(ps -aux | grep nginx | awk '{print $4}')
+
+# If the Nginx web server's memory usage exceeds 512MB, restart it
+if [[ $nginx_memory_usage -gt 512000 ]]; then
+  sudo systemctl restart nginx
+fi
